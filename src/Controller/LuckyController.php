71051e04078bcc580fe4db1eaaa0b3223179456a
Stas <s.areshin@lerpal.com>
2018-04-18 12:25:02 +0300

    Rendering a Template

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -9,10 +9,10 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController
+class LuckyController extends Controller
 {
     /**
      * @Route("lucky/number")
@@ -20,6 +20,9 @@ class LuckyController
     public function number()
     {
         $number = mt_rand(0, 100);
-        return new Response('<html><body>Lucky number: ' . $number . '</body></html>');
+
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+        ));
     }
 }
