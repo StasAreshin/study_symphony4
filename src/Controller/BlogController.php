c50673bff95b28cd3d2c86a33bfcd39e263ee423
Stas <s.areshin@lerpal.com>
2018-04-18 16:31:55 +0300

    Route /blog/{placeholder}

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018-04-18
+ * Time: 13:36
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends Controller
+{
+    /**
+     * Matches /blog exactly
+     *
+     * @Route("/blog", name="blog_list")
+     */
+    public function list()
+    {
+//        return $this->render('base.html.twig', array(
+//            'title' => 'Some title',
+//            'body' => 'BodY',
+//        ));
+        return new Response("<body>blog_list</body>");
+    }
+
+    /**
+     * Metches /blog/*
+     *
+     * @Route("/blog/{placeholder}", name="blog_show")
+     * @param $placeholder
+     * @return Response
+     */
+    public function show($placeholder)
+    {
+        // $slug will equal the dynamic part of the URL
+        // e.g. at /blog/yay-routing, then $slug='yay-routing'
+
+        return new Response('<body>placeholder parameter: ' . $placeholder . '</body>');
+    }
+}
